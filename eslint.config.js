4432213aeef8f62324366949eeca5864cabd30f6
Kaden Griffith <kadengriffith@gmail.com>
2025-10-08 17:52:53 -0600

    chore: fix dependency issues
    chore: update ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,18 @@
 import withNuxt from './.playground/.nuxt/eslint.config.mjs'
 
-export default withNuxt()
+export default withNuxt([{
+  rules: {
+    'no-unused-vars': [
+      'warn',
+      {
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'vue/no-multiple-template-root': 'off',
+    'vue/html-self-closing': 'off',
+    '@typescript-eslint/no-dynamic-delete': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+}])
